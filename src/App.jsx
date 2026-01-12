d443940403ae08daff610e7f9dc2bf6a7b6d27bf
Cyprian <kamaucyprian@gmail.com>
2026-01-12 11:29:33 +0300

    date updates

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const LandingPage = () => {
         <center>
           <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
           <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-            © 2025{" "}
+            © 2026{" "}
             <a href="https://flowbite.com/" className="hover:underline">
               Cyprian Kamau
             </a>
@@ -42,7 +42,7 @@ const ProjectPageLayout = () => (
       <center>
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-          © 2023{" "}
+          © 2026{" "}
           <a href="https://flowbite.com/" className="hover:underline">
             Cyprian Kamau
           </a>
